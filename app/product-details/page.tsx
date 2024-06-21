99d346b0f1cf6e4b8d0ce5eb72775a08da5588a9
github-usman <usmantoyou@gmail.com>
2024-06-21 19:17:06 +0530

    all nextjs code move to frontend folder

--- app/product-details/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-        <main className="wrapper" style={{maxWidth:'800px',width:'100%',height:'400px',backgroundColor:'pink'}}>
-            <p>Product Details are here</p>
-        </main>
-);
-}
